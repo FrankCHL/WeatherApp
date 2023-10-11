ac607aa6f9bdd83aeaa6c2dd1e69dbfe4cdfb5a4
Vadim <vadim036uu@gmail.com>
2023-10-11 20:44:46 +0300

    version 1.1.1 remove: - old NEGRO icon add: - new icon

--- app/src/main/java/com/meteo/programm/weatherapp/WeatherApp.kt
+++ app/src/main/java/com/meteo/programm/weatherapp/WeatherApp.kt
@@ -1,14 +1,27 @@
 package com.meteo.programm.weatherapp
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class WeatherApp : MainActivity() {
+class WeatherApp : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startKoin(){}
-
+        setContent {
+            Start_screen()
+        }
 
     }
+}
+@Preview
+@Composable
+fun Start_screen ()
+{
+
 }
